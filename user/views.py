2ccde26dc6b882520af75d49b8af98a00516db20
Jorge Escobar <j@jorge3.com>
2019-08-29 09:09:10 -0400

    checkpoint

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
-from quart import Blueprint, current_app, render_template
+from quart import Blueprint, current_app, render_template, request
 from sqlalchemy.sql import select
+from werkzeug.security import generate_password_hash
 
 from user.models import user_table
 
@@ -8,4 +9,21 @@ user_app = Blueprint("user_app", __name__)
 
 @user_app.route("/register", methods=["GET", "POST"])
 async def register():
-    return await render_template("user/register.html")
+    error = None
+    user_email = None
+    user_password = None
+    if request.method == "POST":
+        form = await request.form
+        user_email = form.get("user_email")
+        user_password = form.get("user_password")
+        if not user_email or not user_password:
+            error = "Please enter email and password"
+        else:
+            # check if the user exists
+            # register the user
+            breakpoint()
+            hashed_password = generate_password_hash(form.get("password"))
+            pass
+    return await render_template(
+        "user/register.html", error=error, user_email=user_email
+    )
