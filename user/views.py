3759052d24bfc590e7c229a79c2c34a20569a071
Jorge Escobar <j@jorge3.com>
2019-08-30 18:09:06 -0400

    preparing for insert

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from quart import Blueprint, current_app, render_template, request
+from passlib.hash import pbkdf2_sha256
 
 from user.models import user_table
 
@@ -8,18 +9,22 @@ user_app = Blueprint("user_app", __name__)
 @user_app.route("/register", methods=["GET", "POST"])
 async def register() -> str:
     error: str = ""
-    user_email: str = ""
+    user_username: str = ""
     user_password: str = ""
     if request.method == "POST":
-        form = await request.form
-        user_email = form.get("user_email")
-        user_password = form.get("user_password")
-        if not user_email or not user_password:
-            error = "Please enter email and password"
+        form: dict = await request.form
+        user_username = form.get("user_username", "")
+        user_password = form.get("user_password", "")
+        if not user_username or not user_password:
+            error = "Please enter username and password"
         else:
             # check if the user exists
             # register the user
-            pass
+            hash: str = pbkdf2_sha256.hash(user_password)
+            conn = current_app.sac
+            stmt = user_table.insert().values(username=user_username, password=hash)
+            result = await conn.execute(stmt)
+            await conn.execute("commit")
     return await render_template(
-        "user/register.html", error=error, user_email=user_email
+        "user/register.html", error=error, user_username=user_username
     )
