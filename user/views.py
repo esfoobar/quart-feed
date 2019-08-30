8a715d73bbb9a85695a823337375567c2f85cd2e
Jorge Escobar <j@jorge3.com>
2019-08-30 16:49:34 -0400

    Adding mypy to pipenv; basic validation

--- user/views.py
+++ user/views.py
@@ -1,6 +1,4 @@
 from quart import Blueprint, current_app, render_template, request
-from sqlalchemy.sql import select
-from werkzeug.security import generate_password_hash
 
 from user.models import user_table
 
@@ -9,9 +7,9 @@ user_app = Blueprint("user_app", __name__)
 
 @user_app.route("/register", methods=["GET", "POST"])
 async def register():
-    error = None
-    user_email = None
-    user_password = None
+    error: str = ""
+    user_email: str = ""
+    user_password: str = ""
     if request.method == "POST":
         form = await request.form
         user_email = form.get("user_email")
@@ -21,8 +19,6 @@ async def register():
         else:
             # check if the user exists
             # register the user
-            breakpoint()
-            hashed_password = generate_password_hash(form.get("password"))
             pass
     return await render_template(
         "user/register.html", error=error, user_email=user_email
