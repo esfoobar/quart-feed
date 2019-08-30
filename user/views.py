c47093fa5cbc40ba8ef01b603d2bb46bc2e20cc5
Jorge Escobar <j@jorge3.com>
2019-08-30 18:11:07 -0400

    updating to username field

--- user/views.py
+++ user/views.py
@@ -8,18 +8,18 @@ user_app = Blueprint("user_app", __name__)
 @user_app.route("/register", methods=["GET", "POST"])
 async def register() -> str:
     error: str = ""
-    user_email: str = ""
+    user_username: str = ""
     user_password: str = ""
     if request.method == "POST":
         form = await request.form
-        user_email = form.get("user_email")
+        user_username = form.get("user_username")
         user_password = form.get("user_password")
-        if not user_email or not user_password:
-            error = "Please enter email and password"
+        if not user_username or not user_password:
+            error = "Please enter username and password"
         else:
             # check if the user exists
             # register the user
             pass
     return await render_template(
-        "user/register.html", error=error, user_email=user_email
+        "user/register.html", error=error, user_username=user_username
     )
