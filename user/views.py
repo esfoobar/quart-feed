7c8717666d273109601a0a9fb3c3508e36687d81
Jorge Escobar <j@jorge3.com>
2019-09-03 19:02:19 -0400

    completed step-4

--- user/views.py
+++ user/views.py
@@ -1,6 +1,18 @@
-from quart import Blueprint, current_app, render_template, request, session
+from quart import (
+    Blueprint,
+    current_app,
+    render_template,
+    request,
+    session,
+    redirect,
+    url_for,
+)
 from passlib.hash import pbkdf2_sha256
 import uuid
+from typing import Union, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from quart.wrappers.response import Response
 
 from user.models import user_table
 
@@ -8,7 +20,7 @@ user_app = Blueprint("user_app", __name__)
 
 
 @user_app.route("/register", methods=["GET", "POST"])
-async def register() -> str:
+async def register() -> Union[str, "Response"]:
     error: str = ""
     username: str = ""
     password: str = ""
@@ -43,9 +55,64 @@ async def register() -> str:
             stmt = user_table.insert().values(username=username, password=hash)
             result = await conn.execute(stmt)
             await conn.execute("commit")
+            return redirect(url_for(".login"))
         else:
             session["csrf_token"] = str(csrf_token)
 
     return await render_template(
         "user/register.html", error=error, username=username, csrf_token=csrf_token
     )
+
+
+@user_app.route("/login", methods=["GET", "POST"])
+async def login() -> str:
+    error: str = ""
+    username: str = ""
+    password: str = ""
+    csrf_token: uuid.UUID = uuid.uuid4()
+
+    if request.method == "GET":
+        session["csrf_token"] = str(csrf_token)
+
+    if request.method == "POST":
+        form: dict = await request.form
+        username = form.get("username", "")
+        password = form.get("password", "")
+
+        if not username or not password:
+            error = "Please enter username and password"
+
+        if session.get("csrf_token") != form.get("csrf_token"):
+            error = "Invalid POST contents"
+
+        # check if the user exists
+        conn = current_app.sac
+        stmt = user_table.select().where(user_table.c.username == form.get("username"))
+        result = await conn.execute(stmt)
+        row = await result.fetchone()
+        if not row:
+            error = "User not found"
+
+        # check the password
+        if not pbkdf2_sha256.verify(password, row.password):
+            error = "User not found"
+
+        if not error:
+            # login the user
+            del session["csrf_token"]
+            session["user_id"] = row.id
+            session["username"] = row.username
+            return "User logged in"
+        else:
+            session["csrf_token"] = str(csrf_token)
+
+    return await render_template(
+        "user/login.html", error=error, username=username, csrf_token=csrf_token
+    )
+
+
+@user_app.route("/logout", methods=["GET"])
+async def logout() -> "Response":
+    del session["user_id"]
+    del session["username"]
+    return redirect(url_for(".login"))
