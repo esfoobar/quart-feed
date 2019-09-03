4a87861695c0807ab03cffac597b4a3b2f326a1d
Jorge Escobar <j@jorge3.com>
2019-09-03 17:28:39 -0400

    updating form

--- user/views.py
+++ user/views.py
@@ -8,18 +8,16 @@ user_app = Blueprint("user_app", __name__)
 @user_app.route("/register", methods=["GET", "POST"])
 async def register() -> str:
     error: str = ""
-    user_username: str = ""
-    user_password: str = ""
+    username: str = ""
+    password: str = ""
     if request.method == "POST":
         form = await request.form
-        user_username = form.get("user_username")
-        user_password = form.get("user_password")
-        if not user_username or not user_password:
+        username = form.get("username")
+        password = form.get("password")
+        if not username or not password:
             error = "Please enter username and password"
         else:
             # check if the user exists
             # register the user
             pass
-    return await render_template(
-        "user/register.html", error=error, user_username=user_username
-    )
+    return await render_template("user/register.html", error=error, username=username)
