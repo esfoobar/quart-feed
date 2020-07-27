ed706f5f247f730e558639e07419b239981d1650
Jorge Escobar <j@jorge3.com>
2020-07-27 13:53:29 -0400

    all tests passing

--- user/decorators.py
+++ user/decorators.py
@@ -4,9 +4,9 @@ from quart import session, request, redirect, url_for
 
 def login_required(f):
     @wraps(f)
-    def decorated_function(*args, **kwargs):
+    async def decorated_function(*args, **kwargs):
         if session.get("username") is None:
             return redirect(url_for("user_app.login", next=request.url))
-        return f(*args, **kwargs)
+        return await f(*args, **kwargs)
 
     return decorated_function
